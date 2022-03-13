b406ca47a7bd7ae04eef18a46efff02163682b4e
Mohd Faiz Ansari <mohdfaiz.afk@gmail.com>
2022-03-13 20:23:00 +0530

    Added CRUD operation of frontend...

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Joi from "joi-browser";
-import { getGenres } from "../services/fakeGenreService";
-import { getMovie, saveMovie } from "../services/fakeMovieService";
-import Form from './common/form';
+import { getGenres } from "../services/genreService";
+import { getMovie, saveMovie } from "../services/movieService";
+import Form from "./common/form";
 
 class MovieForm extends Form {
   state = {
@@ -21,18 +21,27 @@ class MovieForm extends Form {
       .max(10)
       .label("Daily Rental Rate"),
   };
-  componentDidMount() {
-    const genres = getGenres();
+  async populateGenre() {
+    const { data: genres } = await getGenres();
     this.setState({ genres });
-
-    const movieId = this.props.match.params.id;
-    if (movieId === "new") return;
-
-    const movie = getMovie(movieId);
-    if (!movie) return this.props.history.replace("/not-found");
-
-    this.setState({ data: this.mapToViewModel(movie) });
   }
+  async populateMovie() {
+    try {
+      const movieId = this.props.match.params.id;
+      if (movieId === "new") return;
+      const { data: movie } = await getMovie(movieId);
+
+      this.setState({ data: this.mapToViewModel(movie) });
+    } catch (ex) {
+      if (ex.response && ex.response.status === 404)
+        return this.props.history.replace("/not-found");
+    }
+  }
+  async componentDidMount() {
+    await this.populateGenre();
+    await this.populateMovie();
+  }
+
   mapToViewModel(movie) {
     return {
       _id: movie._id,
@@ -42,8 +51,8 @@ class MovieForm extends Form {
       dailyRentalRate: movie.dailyRentalRate,
     };
   }
-  doSubmit = () => {
-    saveMovie(this.state.data);
+  doSubmit = async () => {
+    await saveMovie(this.state.data);
     this.props.history.push("/movies");
   };
   render() {
--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,10 +1,40 @@
 import http from "./httpService";
+import config from "../config.json";
 
-const apiEndpoint = "http://localhost:3900/api/movies";
+const apiEndpoint = config.apiUrl + "/movies";
+
+function movieUrl (id){
+  return `${apiEndpoint}/${id}`
+}
 
 export function getMovies() {
   return http.get(apiEndpoint);
 }
+export async function getMovie(movieId) {
+  return http.get(movieUrl(movieId));
+}
+
+export function saveMovie(movie) {
+  if(movie._id){
+    const body = {...movie}
+    delete body._id;
+    return http.put(movieUrl(movie._id), body)
+  }
+  return http.post(apiEndpoint,movie)
+  // const movies = await getMovies();
+  // let movieInDb = movies.find((m) => m._id === movie._id) || {};
+  // movieInDb.name = movie.title;
+  // movieInDb.genre = genresAPI.genres.find((g) => g._id === movie.genreId);
+  // movieInDb.numberInStock = movie.numberInStock;
+  // movieInDb.dailyRentalRate = movie.dailyRentalRate;
+
+  // if (!movieInDb._id) {
+  //   movieInDb._id = Date.now().toString();
+  //   movies.push(movieInDb);
+  // }
+
+  // return movieInDb;
+}
 export function deleteMovie(movieId) {
-  return http.delete(apiEndpoint + "/" + movieId);
+  return http.delete(movieUrl(movieId));
 }
